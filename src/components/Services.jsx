d07d4fe00fb37ff09abf57e0db6d11627c890f14
Sahillrathore <sahil921031@gmail.com>
2024-10-05 23:47:51 +0530

    .

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div>
+        <div>
+            <h4 className='capitalize text-4xl'>Services that i offer</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nesciunt facere in magnam dolor aliquam a, fugiat ducimus </p>
+        </div>
+
+        <div></div>
+    </div>
   )
 }
 
