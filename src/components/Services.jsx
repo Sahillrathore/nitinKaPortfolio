82e16c7de8d88b699a01f15f3ecb5ef141a53408
Sahillrathore <sahil921031@gmail.com>
2024-10-05 23:49:49 +0530

    servs

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 
 const Services = () => {
-  return (
-    <div>
+    return (
         <div>
-            <h4 className='capitalize text-4xl'>Services that i offer</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nesciunt facere in magnam dolor aliquam a, fugiat ducimus </p>
-        </div>
+            <div>
+                <h4 className='capitalize text-4xl'>Services that i offer</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nesciunt facere in magnam dolor aliquam a, fugiat ducimus </p>
+            </div>
 
-        <div></div>
-    </div>
-  )
+            <div>
+                <div>
+                    <h5>Frontend Development</h5>
+                </div>
+                <div>
+                    <h5>Website Development</h5>
+                </div>
+                <div>
+                    <h5>Redesign Websites</h5>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Services
