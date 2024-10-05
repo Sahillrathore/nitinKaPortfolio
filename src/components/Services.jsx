b818ca4362a6dc8be5a464b7a19980c5591dc937
Sahillrathore <sahil921031@gmail.com>
2024-10-06 00:09:56 +0530

    service

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,60 +4,36 @@ import { FaSquareArrowUpRight } from "react-icons/fa6";
 
 const Services = () => {
     return (
-        <div className='text-white flex justify-around items-center mt-14 py-8 w-full'>
+        <div className='text-white flex justify-around items- mt-14 py-8 w-full  px-4'>
             <div className='w-[40%]'>
-                <h4 className='capitalize text-4xl font-bold'>Services that i offer</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nesciunt facere in magnam dolor aliquam a, fugiat ducimus </p>
+                <h4 className='capitalize text-5xl font-semibold mb-6 '>Services that i offer</h4>
+                <p className='text-xl font-thin text-gray-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nesciunt facere in magnam dolor aliquam a, fugiat ducimus sadama curs london fuchio namlimoto</p>
             </div>
 
-            <div className='text-5xl font-light'>
-                <div className='flex gap-4 items-center p-3 py-5 border-y border-gray-400'>
-                    <h5>Frontend Development</h5>
-                    <FaSquareArrowUpRight/>
-                </div>
-                <div className='flex gap-4 items-center p-5 border-y border-gray-400'>
-                    <h5>Website Development</h5>
+            <div className='text-5xl font-light w-1/2'>
+                <div className='flex justify-between gap-8 items-center p-3 py-5 border-y border-gray-400'>
+                    <div className='flex items-center gap-4'>
+                        <span className='text-lg font-semibold -mt-3'>01/</span>
+                        <h5>Frontend Development</h5>
+                    </div>
                     <FaSquareArrowUpRight />
-
                 </div>
-                <div className='flex gap-4 items-center p-5 border-y border-gray-400'>
-                    <h5>Redesign Websites</h5>
+                <div className='flex justify-between gap-8 items-center p-3 py-5 border-y border-gray-400'>
+                    <div className='flex items-center gap-4'>
+                        <span className='text-lg font-semibold -mt-3'>01/</span>
+                        <h5>Frontend Development</h5>
+                    </div>
                     <FaSquareArrowUpRight />
-
+                </div>
+                <div className='flex justify-between gap-8 items-center p-3 py-5 border-y border-gray-400'>
+                    <div className='flex items-center gap-4'>
+                        <span className='text-lg font-semibold -mt-3'>01/</span>
+                        <h5>Frontend Development</h5>
+                    </div>
+                    <FaSquareArrowUpRight />
                 </div>
             </div>
-            <div className='text-5xl font-light'>
-                <div className='flex gap-4 items-center p-3 py-5 border-y border-gray-400'>
-                    <h5>Frontend Development</h5>
-                    <FaSquareArrowUpRight />
-                </div>
-                <div className='flex gap-4 items-center p-5 border-y border-gray-400'>
-                    <h5>Website Development</h5>
-                    <FaSquareArrowUpRight />
 
-                </div>
-                <div className='flex gap-4 items-center p-5 border-y border-gray-400'>
-                    <h5>Redesign Websites</h5>
-                    <FaSquareArrowUpRight />
-
-                </div>
-            </div>
-            <div className='text-5xl font-light'>
-                <div className='flex gap-4 items-center p-3 py-5 border-y border-gray-400'>
-                    <h5>Frontend Development</h5>
-                    <FaSquareArrowUpRight />
-                </div>
-                <div className='flex gap-4 items-center p-5 border-y border-gray-400'>
-                    <h5>Website Development</h5>
-                    <FaSquareArrowUpRight />
-
-                </div>
-                <div className='flex gap-4 items-center p-5 border-y border-gray-400'>
-                    <h5>Redesign Websites</h5>
-                    <FaSquareArrowUpRight />
-
-                </div>
-            </div>
         </div>
     )
 }
