5db659408539cebb1bd85455d448074dcbfaa0bf
Sahillrathore <sahil921031@gmail.com>
2024-10-05 23:53:45 +0530

    s

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Services = () => {
     return (
-        <div className='text-white flex justify-between w-full'>
+        <div className='text-white flex justify-around items-center mt-14 py-8 w-full'>
             <div className='w-[40%]'>
-                <h4 className='capitalize text-4xl'>Services that i offer</h4>
+                <h4 className='capitalize text-4xl font-bold'>Services that i offer</h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, nesciunt facere in magnam dolor aliquam a, fugiat ducimus </p>
             </div>
 
